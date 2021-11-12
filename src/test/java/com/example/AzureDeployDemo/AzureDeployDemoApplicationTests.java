cb3c0587b20b51f57cf4329e6f453ea8833ccf52
Namrata <namrata.khadtar@gmail.com>
2021-11-12 18:20:56 +0530

    first commit

---
+++ src/test/java/com/example/AzureDeployDemo/AzureDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureDeployDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
